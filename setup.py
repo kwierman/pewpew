fdc1abbdd6f2894cb7304771cc6bf399ed41e487
Wierman, Kevin <kevin.wierman@pnnl.gov>
2018-01-08 16:51:44 -0800

    hacky bugfix for distutils

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 import pip
-
+import os
 
 this_path = os.path.abspath(os.path.join(__file__,'..'))
 requirements_path = os.path.join(this_path, 'requirements.txt')
